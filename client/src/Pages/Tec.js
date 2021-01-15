ca8ea119553fc47264a7df8b0d0c1d9886a52dc2
RodrigoRK7 <a00824488@itesm.mx>
2021-01-15 01:19:39 -0600

    src

---
+++ client/src/Pages/Tec.js
@@ -0,0 +1,38 @@
+import NavBar from "../Components/Navbar";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  fondo: {
+    backgroundImage:
+      "url(https://tec.mx/sites/default/files/styles/header_full/public/2019-04/EstadioBorregos-Tec-de-Monterrey.jpg?itok=HQUIbkeh)",
+    backgroundSize: "cover",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    minWidth: "100%",
+    minHeight: "100%",
+    position: "absolute",
+    marginTop: "80px",
+    zIndex: "-1",
+  },
+  title: {
+    fontSize: "3rem",
+    color: "white",
+    paddingBottom: "5%",
+    textShadow: "4px 10px 8px black",
+  },
+}));
+function Tec() {
+  const classes = useStyles();
+  return (
+    <div>
+      <NavBar />
+      <div className={classes.fondo}>
+        <div className={classes.title}>
+          <b>TEC</b>
+        </div>
+      </div>
+    </div>
+  );
+}
+export default Tec;
